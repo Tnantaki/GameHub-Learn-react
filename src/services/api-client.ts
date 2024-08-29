35b60fe17c428c4403129398f13d305e101787b0
tnantaki <tinyhave@gmail.com>
2024-08-29 12:05:58 +0700

    Change API Origin

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "https://api.rawg.io/api",
-//   params: {
-//     key: import.meta.env.VITE_RAWG_API_KEY
-//   }
-// })
-
 export default axios.create({
-  baseURL: import.meta.env.VITE_PRIVATE_URL
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: import.meta.env.VITE_RAWG_API_KEY
+  }
 })
+
+// export default axios.create({
+//   baseURL: import.meta.env.VITE_PRIVATE_URL
+// })
