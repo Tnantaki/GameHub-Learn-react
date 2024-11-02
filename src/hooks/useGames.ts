161abc8a643521a09c3ee1952c22002b9d58ee90
tnantaki <tinyhave@gmail.com>
2024-11-02 11:08:23 +0700

    Implement infinite query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,33 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 import APIClient from "../services/api-client";
 
 export interface Game {
-  id: number
-  name: string
-  background_image: string
-  parent_platforms: {platform: Platform}[]
-  metacritic: number
-  rating_top: number
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
 }
 
-const gamesService = new APIClient<Game>('/games')
+const gamesService = new APIClient<Game>("/games");
 
 const useGames = (gameQuery: GameQuery) => {
-  return useQuery({
-    queryKey: ['games', gameQuery],
-    queryFn: () => gamesService.getAll({
-      params: {
-        genres: gameQuery.genre?.id,
-        parent_platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      }
-    }),
+  return useInfiniteQuery({
+    queryKey: ["games", gameQuery],
+    queryFn: ({ pageParam }) =>
+      gamesService.getAll({
+        params: {
+          page: pageParam,
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPage) => {
+      return lastPage.next ? allPage.length + 1 : undefined;
+    },
     staleTime: 24 * 60 * 60 * 1000, // 24h
-  })
-}
+  });
+};
 // const useGames = (gameQuery: GameQuery) =>
 //   useData<Game>(
 //     "/games",
@@ -42,4 +48,4 @@ const useGames = (gameQuery: GameQuery) => {
 //     [gameQuery]
 //   );
 
-export default useGames
+export default useGames;
